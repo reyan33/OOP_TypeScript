804472750eaed97c322e941212ad4bea8f91ef83
Reyan <reyanyenate@gmail.com>
2026-08-18 12:48:18 +0300

    Add rectangle object

--- src/shapes-demo.ts
+++ src/shapes-demo.ts
@@ -1,7 +1,15 @@
 export {};
 
+const rectangle = {
+  x: 550,
+  y: 200,
+  width: 300,
+  height: 200,
+};
+
 const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 ctx.fillStyle = "#e86a33";
-ctx.fillRect(550, 200, 300, 200);
+
+ctx.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
